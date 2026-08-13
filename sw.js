3ddeb91f2dbd2028d3202e79415992da41c22123
bgcarvalho13 <bgcarvalho13@gmail.com>
2026-08-13 18:25:39 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minhas-financas-v8';
+const CACHE = 'minhas-financas-v9';
 const APP_SHELL = [
   './', './index.html', './styles.css', './app.js', './db.js', './seed.js',
   './manifest.json', './assets/icon-192.png', './assets/icon-512.png'
